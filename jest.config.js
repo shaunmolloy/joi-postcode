023cabc8e2a25be789b4da9c0b3db4dd38952fc0
Shaun Molloy <shaunmolloy@gmail.com>
2021-05-08 20:24:38 +0100

    Set transformIgnorePatterns with joi in jest conf

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,7 @@ module.exports = {
   verbose: true,
   rootDir: path.join(__dirname),
   moduleFileExtensions: ['js', 'json'],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!joi)',
+  ],
 };
