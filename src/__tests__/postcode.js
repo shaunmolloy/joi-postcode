51381ae7241cf20089451c7b766870ec6fdf4adf
Shaun Molloy <shaunmolloy@gmail.com>
2021-05-08 19:00:04 +0100

    Update postcode test to check valid & invalid

--- src/__tests__/postcode.js
+++ src/__tests__/postcode.js
@@ -1,5 +1,17 @@
+import JoiPostcode from '../postcode';
+
 describe('postcode', () => {
-    test('it should pass', () => {
-        // to be replaced
-    });
+  const schema = JoiPostcode.string();
+
+  it('should pass when postcode is valid', () => {
+    const { error } = schema.validate('SW1A 2AA');
+    expect(error).toEqual(undefined);
+  });
+
+  it('should fail when postcode is invalid', () => {
+    const valid = schema.validate('SW1A');
+    const [ error ] = valid.error.details;
+    expect(error.type).toEqual('postcode.invalid');
+    expect(error.message).toEqual('Invalid postcode');
+  });
 });
